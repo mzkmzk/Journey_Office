4e3ddb9bda73beaf629a119b8da1502a1682cc5f
Mai ZhiKun <maizk@haplox.com>
2016-07-28 08:49:04 +0800

    webpack init

---
+++ Src/Containers/Index/Index.js
@@ -0,0 +1,22 @@
+import React , { Component } from 'react'
+import { render } from 'react-dom'
+
+import Header from '../../Components/Header/Header'
+
+import './Index.scss'
+
+export default class Index extends Component {
+
+    render() {
+        return (
+            <article>
+                <Header></Header>
+            </article>
+        )
+    }
+}
+
+
+
+
+
